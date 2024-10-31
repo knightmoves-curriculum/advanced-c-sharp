0bb65eeee924800af64efd0ca02244545db0f951
David Kessler <39646674+pairing4good@users.noreply.github.com>
2024-10-31 10:36:51 +0000

    added boom

--- MyFirstApi/Conrollers/WeatherForecastController.cs
+++ MyFirstApi/Conrollers/WeatherForecastController.cs
@@ -4,14 +4,33 @@ namespace MyFirstApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController: ControllerBase
+    public class WeatherForecastController : ControllerBase
     {
-        private  static List<WeatherForecast> forecast = new List<WeatherForecast>();
-        
-        [HttpGet]    
+        private static List<WeatherForecast> forecast = new List<WeatherForecast>();
+
+        [HttpGet]
         public IEnumerable<WeatherForecast> ListAllWeatherForecasts()
         {
             return forecast;
+        }
+
+        [HttpGet("boom")]
+        public IActionResult GoBoom()
+        {
+            Response.Headers.Append("API-Version", "1.0");
+            Response.Headers.Append("Something", "else");
+            
+            try
+            {
+                throw new Exception("Boom!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new {
+                    Message = ex.Message,
+                    SupportContact = "support@knightmoves.org"
+                });
+            }
         }
 
         [HttpPost]
@@ -21,10 +40,10 @@ namespace MyFirstApi.Controllers
             return Created($"/weatherforecast/{forecast.Count - 1}", weatherForecast);
         }
 
-        [HttpGet("{id}")] 
+        [HttpGet("{id}")]
         public IActionResult FindById(int id)
         {
-            if(id > (forecast.Count - 1))
+            if (id > (forecast.Count - 1))
             {
                 return NotFound();
             }
@@ -34,7 +53,7 @@ namespace MyFirstApi.Controllers
         [HttpPut("{id}")]
         public IActionResult UpdateById([FromBody] WeatherForecast weatherForecast, [FromRoute] int id)
         {
-            if(id > (forecast.Count - 1))
+            if (id > (forecast.Count - 1))
             {
                 return NotFound();
             }
@@ -45,7 +64,7 @@ namespace MyFirstApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult Remove(int id)
         {
-            if(id > (forecast.Count - 1))
+            if (id > (forecast.Count - 1))
             {
                 return NotFound();
             }
