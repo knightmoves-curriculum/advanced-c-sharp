d9a074d33f85f32c2cb37182827dadd97ec2f927
David Kessler <39646674+pairing4good@users.noreply.github.com>
2024-12-04 20:46:59 +0000

    save lesson 3

--- MyFirstApi/Controllers/WeatherForecastController.cs
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -6,23 +6,19 @@ namespace MyFirstApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController: ControllerBase
     {
-        private string[] summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static List<WeatherForecast> forecast = new List<WeatherForecast>();
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
             return forecast;
+        }
+
+        [HttpPost]
+        public WeatherForecast Post([FromBody] WeatherForecast weatherForecast)
+        {
+            forecast.Add(weatherForecast);
+            return weatherForecast;
         }
     }
 }
