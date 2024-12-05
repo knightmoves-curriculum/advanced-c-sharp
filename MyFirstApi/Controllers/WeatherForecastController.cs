a526dcaeefea3db48575cbf8beb18edf96f13ba7
David Kessler <39646674+pairing4good@users.noreply.github.com>
2024-12-05 15:58:45 +0000

    baseline

--- MyFirstApi/Controllers/WeatherForecastController.cs
+++ MyFirstApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Models;
 
 namespace MyFirstApi.Controllers
 {
@@ -6,67 +7,47 @@ namespace MyFirstApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController: ControllerBase
     {
-        private static List<WeatherForecast> forecast = new List<WeatherForecast>();
+        private static WeatherForecastRepository repository = new WeatherForecastRepository();
 
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(forecast);
-        }
-
-        [HttpGet("boom")]
-        public IActionResult GetBoom()
-        {
-            try
-            {
-                throw new Exception("Boom!");
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new
-                {
-                    Status = 500,
-                    Error = "Internal Server Error",
-                    Message = ex.Message,
-                    SupportContact = "support@knightmove.org"
-                });
-            }
+            return Ok(repository.FindAll());
         }
 
         [HttpGet("{id}")]
         public IActionResult FindById(int id)
         {
-            if(id > (forecast.Count - 1))
+            if(id > (repository.FindAll().Count - 1))
             {
                 return NotFound();
             }
-            var weatherForecast = forecast[id];
+            var weatherForecast = repository.FindById(id);
             return Ok(weatherForecast);
         }
 
         [HttpPost]
         public IActionResult Post([FromBody] WeatherForecast weatherForecast)
         {
-            forecast.Add(weatherForecast);
-            return Created($"/weatherforecast/{forecast.Count - 1}", weatherForecast);
+            repository.Save(weatherForecast);
+            return Created($"/weatherforecast/{repository.FindAll().Count - 1}", weatherForecast);
         }
 
         [HttpPut("{id}")]
         public WeatherForecast Update([FromBody] WeatherForecast weatherForecast, [FromRoute] int id)
         {
-            forecast[id] = weatherForecast;
+            repository.Update(id, weatherForecast);
             return weatherForecast;
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            if(id > (forecast.Count - 1))
+            if(id > (repository.FindAll().Count - 1))
             {
                 return NotFound();
             }
-            var weatherForecast = forecast[id];
-            forecast.Remove(weatherForecast);
+            var weatherForecast = repository.RemoveById(id);
             return Ok(weatherForecast);
         }
     }
---
+++ MyFirstApi/Models/WeatherForecastRepository.cs
@@ -0,0 +1,41 @@
+namespace MyFirstApi.Models
+{
+    public class WeatherForecastRepository
+    {
+        private List<WeatherForecast> forecast;
+
+        public WeatherForecastRepository()
+        {
+            forecast = new List<WeatherForecast>();
+        }
+
+        public WeatherForecast Save(WeatherForecast weatherForecast)
+        {
+            forecast.Add(weatherForecast);
+            return weatherForecast;
+        }
+
+        public WeatherForecast Update(int id, WeatherForecast weatherForecast)
+        {
+            forecast[id] = weatherForecast;
+            return weatherForecast;
+        }
+
+        public List<WeatherForecast> FindAll()
+        {
+            return forecast;
+        }
+
+        public WeatherForecast FindById(int id)
+        {
+            return forecast[id];
+        }
+
+        public WeatherForecast RemoveById(int id)
+        {
+            var weatherForecast = forecast[id];
+            forecast.Remove(weatherForecast);
+            return weatherForecast;
+        }
+    }
+}
